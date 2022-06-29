e0be5f4adb27ffbb4619e7f74a676d5b1560fdbb
hrms-jobs <hrms-jobs@intimetec.com>
2022-06-29 15:47:24 +0530

    updating ./roboshop-shell-script

--- component/mongodb.sh
+++ component/mongodb.sh
@@ -15,7 +15,7 @@ systemctl start mongod
 systemctl restart mongod
 curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
 cd /tmp
-unzip -o mongodb.zip
+unzip  mongodb.zip
 cd mongodb-main
 mongo < catalogue.js
 mongo < users.js
