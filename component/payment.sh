efc7c03226390709fa0d70cdb9846a4626bda669
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-05 17:23:43 +0530

    updating ./roboshop-shell-script

--- component/payment.sh
+++ component/payment.sh
@@ -1,1 +1,16 @@
-echo install payment componetnt
+yum install python36 gcc python3-devel -y
+useradd roboshop
+cd /home/roboshop
+rm -rf payment
+
+curl -L -s -o /tmp/payment.zip "https://github.com/roboshop-devops-project/payment/archive/main.zip"
+unzip -o /tmp/payment.zip
+mv payment-main payment
+cd /home/roboshop/payment
+pip3 install -r requirements.txt
+
+#sed -i -e 's/CARTHOST/cart.roboshop.internal/' -e 's/USERHOST/user.roboshop.internal/' -e 's/AMQPHOST/rabitmq.roboshop.internal/' /home/roboshop/payment/systemd.service
+#mv /home/roboshop/payment/systemd.service /etc/systemd/system/payment.service
+#systemctl daemon-reload
+#systemctl enable payment
+#systemctl restart payment
