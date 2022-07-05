781265dee63d488ab267a84a42c96c70ff9cd8f5
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-05 14:21:03 +0530

    updating ./roboshop-shell-script

--- component/redis.sh
+++ component/redis.sh
@@ -1,1 +1,9 @@
-echo install redisl componetnt
+ curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+ yum install redis-6.2.7 -y
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+#2. Update the `bind` from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
+
+# Start Redis Database
+systemctl enable redis
+systemctl start redis
