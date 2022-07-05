ade685829e48bdf5a81a01a7e72b972fce88dcdb
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-05 14:34:07 +0530

    updating ./roboshop-shell-script

--- component/cart.sh
+++ component/cart.sh
@@ -1,1 +1,19 @@
-echo install cart componetnt
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+curl -s -L -o /tmp/cart.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip"
+cd /home/roboshop
+unzip /tmp/cart.zip
+mv cart-main cart
+cd cart
+
+#Update SystemD service file
+
+  #        Update `REDIS_ENDPOINT` with REDIS server IP Address
+ #         Update `CATALOGUE_ENDPOINT` with Catalogue server IP address
+
+sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/' -e 's/CATALOGUE_ENDPOINT/catalogue.roboshop.internal/' /home/roboshop/cart/systemd.service
+mv /home/roboshop/cart/systemd.service /etc/systemd/system/cart.service
+systemctl daemon-reload
+systemctl restart cart
+systemctl enable cart
