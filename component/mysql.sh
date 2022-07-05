39276b770feefcbfa9e7d4000ca63e1cc94234aa
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-05 16:11:16 +0530

    updating ./roboshop-shell-script

--- component/mysql.sh
+++ component/mysql.sh
@@ -7,7 +7,7 @@ systemctl start mysqld
 
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
-
+uninstall plugin validate_password; | mysql --connect-expired-password -uroot -p"${MYSQL_PASSWORD}"
 #
 ##grep temp /var/log/mysqld.log
 #
@@ -20,7 +20,7 @@ echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql 
 ##Once after login to MySQL prompt then run this SQL Command.
 #
 ##sql
-##uninstall plugin validate_password;
+
 #
 #
 ##As per the architecture diagram, MySQL is needed by
