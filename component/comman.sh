9a21f417adf3c0650749d6e569b95e5bd3f0f464
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-19 22:10:53 +0530

    updating ./roboshop-shell-script

--- component/comman.sh
+++ component/comman.sh
@@ -45,7 +45,7 @@ fi
 CHECK_STAT $?
 
 PRINT "Downloading ${COMPONENT} content"
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/cart/archive/main.zip" &>>${LOG}
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/roboshop-devops-project/${COMPONENT}/archive/main.zip" &>>${LOG}
 CHECK_STAT $?
 
 
