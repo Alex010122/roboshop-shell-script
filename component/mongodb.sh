66ccf5349fe7adbabd11ab8071a0a3d0f5297c46
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-20 00:04:23 +0530

    updating ./roboshop-shell-script

--- component/mongodb.sh
+++ component/mongodb.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 source component/comman.sh
 CHECK_ROOT
-curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo
 
-yum install -y mongodb-org
-systemctl enable mongod
-systemctl start mongod
+PRINT " Configure yum repos "
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/roboshop-devops-project/mongodb/main/mongo.repo &>>${LOG}
+CHECK_STAT $?
 
+PRINT " Install mongodb"
+yum install -y mongodb-org &>>${LOG}
+CHECK_STAT $?
+
+PRINT " Configure Mongodb service"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+CHECK_STAT $?
 
-#1. Update Listen IP address from 127.0.0.1 to 0.0.0.0 in config file
-#Config file: `/etc/mongod.conf`
-#then restart the service
-
+PRINT "Start MongoDB"
+systemctl enable mongod &>>${LOG}  && systemctl restart mongod &>>${LOG}
+CHECK_STAT $?
 
 systemctl restart mongod
-curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip"
-cd /tmp
-unzip -o mongodb.zip
-cd mongodb-main
-mongo < catalogue.js
-mongo < users.js
-##Every Database needs the schema to be loaded for the application to work.
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
+PRINT " LOad schema"
+cd /tmp && unzip -o mongodb.zip &>>${LOG} && cd mongodb-main && mongo < catalogue.js &>>${LOG} && mongo < users.js &>>${LOG}
+CHECK_STAT $?
+
