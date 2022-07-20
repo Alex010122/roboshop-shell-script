fe905f0e736a63d94250771f0953f7d71bef0fef
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-20 11:51:26 +0530

    updating ./roboshop-shell-script

--- component/redis.sh
+++ component/redis.sh
@@ -1,11 +1,21 @@
 source component/comman.sh
 CHECK_ROOT
- curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
- yum install redis-6.2.7 -y
 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+PRINT " setup yum repo"
+ curl -L https://raw.githubusercontent.com/roboshop-devops-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo &>>${LOG}
+ CHECK_STAT $?
+
+ PRINT " install redis"
+ yum install redis-6.2.7 -y &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Configure redis config"
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${LOG}
+CHECK_STAT $?
+
 #2. Update the `bind` from `127.0.0.1` to `0.0.0.0` in config file `/etc/redis.conf` & `/etc/redis/redis.conf`
 
-# Start Redis Database
-systemctl enable redis
-systemctl start redis
+
+
+PRINT "Start Redis Database"
+systemctl enable redis &>>${LOG} && systemctl restart redis &>>${LOG}
