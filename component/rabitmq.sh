8f83fdac4ed74a3d0983563d6ad22427c53b3281
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-20 12:48:29 +0530

    updating ./roboshop-shell-script

--- component/rabitmq.sh
+++ component/rabitmq.sh
@@ -1,8 +1,18 @@
 source component/comman.sh
 CHECK_ROOT
+
+PRINT " Setup yum repos"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
+CHECK_STAT $?
+
+PRINT " Install Erlang"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+CHECK_STAT $?
+
+PRINT " Start Rabitmq service"
 yum install rabbitmq-server -y
+CHECK_STAT $?
+
 systemctl enable rabbitmq-server
 systemctl start rabbitmq-server
 rabbitmqctl add_user roboshop roboshop123
