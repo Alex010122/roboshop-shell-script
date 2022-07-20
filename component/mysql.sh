fa198a25a1fc4f83260fa2e6f317e38685a8013d
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-20 11:46:15 +0530

    updating ./roboshop-shell-script

--- component/mysql.sh
+++ component/mysql.sh
@@ -1,34 +1,42 @@
-source component/comman.sh
+source components/comman.sh
+
 CHECK_ROOT
 
 if [ -z "${MYSQL_PASSWORD}" ]; then
-  echo " need variable input "
-
-exit 1
+  echo "Need MYSQL_PASSWORD env variable"
+  exit 1
 fi
 
-PRINT " configure yum repos"
+PRINT "Configure YUM Repos"
 curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
 CHECK_STAT $?
 
-PRINT "install mysql"
+PRINT "Install MySQL"
 yum install mysql-community-server -y &>>${LOG}
-systemctl enable mysqld && systemctl start mysqld
+systemctl enable mysqld &>>${LOG} && systemctl start mysqld &>>${LOG}
 CHECK_STAT $?
-
 
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+
 echo show databases | mysql -uroot -p"${MYSQL_PASSWORD}" &>>${LOG}
 if [ $? -ne 0 ]; then
-  PRINT " reset root password"
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" ${LOG}
-CHECK_STAT $?
-fi 
-exit
-echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -p"${MYSQL_PASSWORD}"
+  PRINT "RESET Root Password"
+  echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}" &>>${LOG}
+  CHECK_STAT $?
+fi
 
-curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
-cd /tmp
-cd mysql-main
-unzip -o mysql.zip
-mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql
+echo show plugins  | mysql -uroot -p"${MYSQL_PASSWORD}" 2>>${LOG} | grep validate_password &>>${LOG}
+if [ $? -eq 0 ]; then
+  PRINT "Uninstall Password Validate Plugin"
+  echo "uninstall plugin validate_password;" | mysql -uroot -p"${MYSQL_PASSWORD}" &>>${LOG}
+  CHECK_STAT $?
+fi
+
+
+PRINT "Download Schema"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>${LOG}
+CHECK_STAT $?
+
+PRINT "Load Schema"
+cd /tmp && unzip -o mysql.zip &>>${LOG} && cd mysql-main && mysql -u root -p"${MYSQL_PASSWORD}" <shipping.sql &>>${LOG}
+CHECK_STAT $?
