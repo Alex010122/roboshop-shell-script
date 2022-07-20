6bb3310c83e5d7fde7b2228cf3187e1376cc1ac4
hrms-jobs <hrms-jobs@intimetec.com>
2022-07-20 10:30:41 +0530

    updating ./roboshop-shell-script

--- component/mysql.sh
+++ component/mysql.sh
@@ -1,12 +1,28 @@
 source component/comman.sh
 CHECK_ROOT
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo
-yum install mysql-community-server -y
-systemctl enable mysqld
-systemctl start mysqld
+
+if [ -z "${MYSQL_PASSWORD}" ]; then
+  echo " need variable input "
+
+exit 1
+fi
+
+PRINT " configure yum repos"
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/roboshop-devops-project/mysql/main/mysql.repo &>>${LOG}
+CHECK_STAT $?
+
+PRINT "install mongodb"
+yum install mysql-community-server -y &>>${LOG}
+systemctl enable mysqld && systemctl start mysqld
+CHECK_STAT $?
+
 
 MYSQL_DEFAULT_PASSWORD=$(grep 'temporary password' /var/log/mysqld.log | awk '{print $NF}')
+PRINT " reset root password"
 echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';" | mysql --connect-expired-password -uroot -p"${MYSQL_DEFAULT_PASSWORD}"
+CHECK_STAT $?
+
+exit
 echo "uninstall plugin validate_password;" | mysql --connect-expired-password -uroot -p"${MYSQL_PASSWORD}"
 
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
